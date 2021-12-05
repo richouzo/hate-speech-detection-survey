31a0bf23e896d7b0762a3df893601129f235234e
Richard Huang <huang.richard.17@gmail.com>
2021-12-05 17:09:58 -0500

    Change transformers

--- models/Transformers.py
+++ models/Transformers.py
@@ -16,8 +16,11 @@ class DistillBert(nn.Module):
         )
 
         self.bert = AutoModel.from_pretrained(pretrained_model_name, config=config)
-        self.classifier = nn.Linear(config.hidden_size, output_dim)
+        hidden_size = config.hidden_size
+        self.linear1 = nn.Linear(hidden_size, hidden_size//2)
+        self.linear2 = nn.Linear(hidden_size//2, num_classes)
         self.dropout = nn.Dropout(dropout)
+        self.relu = nn.ReLU()
 
         # Freeze bert layers
         for name, p in self.named_parameters():
@@ -26,21 +29,15 @@ class DistillBert(nn.Module):
 
     def forward(self, x):
         x = x.permute(1, 0)
-        # print('x', x)
         attention_mask = (torch.where(x==0, 0, 1)) # pad_index = tokenizer.convert_tokens_to_ids(tokenizer.pad_token) = 0
-        # print('attention_mask', attention_mask)
         bert_output = self.bert(input_ids=x, attention_mask=attention_mask)
-        # print('bert_output', bert_output)
-        # qzdqz
         # we only need the hidden state here and don't need
         # transformer output, so index 0
         seq_output = bert_output[0]  # (bs, seq_len, dim)
         # mean pooling, i.e. getting average representation of all tokens
         pooled_output = seq_output.mean(axis=1)  # (bs, dim)
         pooled_output = self.dropout(pooled_output)  # (bs, dim)
-        scores = self.classifier(pooled_output).view(-1)  # (bs, num_classes)
+        out = self.relu(self.linear1(pooled_output))  # (bs, num_classes)
+        out = self.linear2(out)  # (bs, num_classes)
 
-        # print('scores', scores.shape)
-
-        return scores
-
+        return out
